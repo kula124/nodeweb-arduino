a0f2d203675ed1b1dc6960254e94ff63b1797dcf
kula <kula124@gmail.com>
2020-10-04 12:22:20 +0200

    adding DeviceRotationEvent logics

---
+++ src/constants/message.js
@@ -0,0 +1,5 @@
+module.exports = {
+  GYRO_START: 'GYRO.START',
+  GYRO_END: 'GYRO.END',
+  GYRO_DATA: 'GYRO.DATA'
+}
--- src/services/socket.js
+++ src/services/socket.js
@@ -1,8 +1,32 @@
 const Server = require('socket.io');
 const io = new Server();
 
+const messageTypes = require('../constants/message')
+//const controller = require('../controller')
+const MotorActions = require('../constants/motor')
+const ServoActions = require('../constants/servo')
+
+const gyroState = {
+    alpha: 0,
+    beta: 0,
+    gamma: 0
+}
+
 io.on('connect', socket => {
-    socket.on('message', data => console.log(data))
+    socket.on('message', data => {
+        console.log(data)
+        const {type, ...deltas} = data
+        switch (type) {
+            case messageTypes.GYRO_DATA:
+                console.log('IN DATA: ', deltas)
+                break;
+            case messageTypes.GYRO_END:
+                console.log('END:')
+                //controller.act(MotorActions.STOP);
+                //controller.act(ServoActions.STOP);
+                break;
+        }
+    })
 })
 
 io.attach(3000, {

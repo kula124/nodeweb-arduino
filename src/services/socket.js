4abc92b9fde4836f17fa068a9e9241a084f03b88
kula <kula124@gmail.com>
2020-10-04 22:53:28 +0200

    termux logics applied

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,10 +1,13 @@
 const Server = require('socket.io');
 const io = new Server();
+const { spawn } = require('child_process')
 
 const messageTypes = require('../constants/message')
 //const controller = require('../controller')
 const MotorActions = require('../constants/motor')
 const ServoActions = require('../constants/servo')
+
+let runner = null
 
 const gyroState = {
     alpha: 0,
@@ -12,16 +15,32 @@ const gyroState = {
     gamma: 0
 }
 
+const Run = () => {
+    runner = spawn('termux-sensor', ['-s KDS3TR Accelerometer', '-d 100'])
+    let firstRun = true
+    runner.stdout.on('data', data => {
+        console.log(data)
+        if (firstRun) {
+            gyroState.alpha = data.AccMeter.alpha
+            gyroState.beta = data.AccMeter.beta
+            gyroState.gamma = data.AccMeter.gamma
+            firstRun = false;
+        }
+    })
+}
+
 io.on('connect', socket => {
     socket.on('message', data => {
-        console.log(data)
+        data = JSON.parse(data)
         const {type, ...deltas} = data
         switch (type) {
-            case messageTypes.GYRO_DATA:
-                console.log('IN DATA: ', deltas)
+            case messageTypes.GYRO_START:
+                console.log('START:')
+                Run()
                 break;
             case messageTypes.GYRO_END:
                 console.log('END:')
+                runner ? runner.kill() : (() => console.log('ayy lmaoo'))()
                 //controller.act(MotorActions.STOP);
                 //controller.act(ServoActions.STOP);
                 break;
---
+++ src/services/test.js
@@ -0,0 +1,7 @@
+setInterval(() => console.log({
+  AccMeter: {
+    alpha: 'a',
+    beta: 'b',
+    gamma: 'g'
+  }
+}), 200)

bd2598ce646a9bf08349d1afc9aedd48afc83cf1
kula <kula124@gmail.com>
2020-10-19 23:16:32 +0200

    J5 MOTOR

--- src/controller.js
+++ src/controller.js
@@ -3,14 +3,16 @@ const motorAction = require('./constants/motor')
 const servoAction = require('./constants/servo')
 const DcMotor = require('./customMotor')
 
-const motor = new DcMotor({
-  // config: five.Motor.SHIELD_CONFIGS.POLOLU_DRV8835_SHIELD,
-  pins: {
-    pwm: process.env.DCMOTOR_PIN_PWM,
-    dir: process.env.DCMOTOR_PIN_D2,
-    cdir: process.env.DCMOTOR_PIN_D1
-  }
-})
+// const motor = new DcMotor({
+//   // config: five.Motor.SHIELD_CONFIGS.POLOLU_DRV8835_SHIELD,
+//   pins: {
+//     pwm: process.env.DCMOTOR_PIN_PWM,
+//     dir: process.env.DCMOTOR_PIN_D2,
+//     cdir: process.env.DCMOTOR_PIN_D1
+//   }
+// })
+
+const motor = new five.Motor([process.env.DCMOTOR_PIN_PWM, process.env.DCMOTOR_PIN_D1, process.env.DCMOTOR_PIN_D2])
 
 const servo = new five.Servo({
   pin: process.env.SERVO_PIN,
